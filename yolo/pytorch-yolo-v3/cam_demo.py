237ee6215397d61723a631ca157adad88cb10d85
Joshua Kristanto <joshuakristanto68@gmail.com>
2019-05-01 17:55:50 -0700

    Added Request

--- yolo/pytorch-yolo-v3/cam_demo.py
+++ yolo/pytorch-yolo-v3/cam_demo.py
@@ -93,102 +93,109 @@ def arg_parse():
 if __name__ == '__main__':
 
     # Code for taking in input for filtering
-    class_filter = input("Enter a object for filtering,(i.e. person, chair, laptop) or Enter 'All' to identify all objects. \n")
-    class_filter = class_filter.lower()
+    cameraPermission = input("We are requesting access to the camera? y = yes and n = no \n")
+    cameraPermission = cameraPermission.lower()
+
+    if cameraPermission == "y":
+        cameraChoice = input("Please Select Camera choice 0 = Internet Camera and 1 = external camera. \n")
+        cameraChoice = int(cameraChoice)
+        class_filter = input("Enter a object for filtering,(i.e. person, chair, laptop) or Enter 'All' to identify all objects. \n")
+        class_filter = class_filter.lower()
     ###########################################
 
-    cfgfile = "cfg/yolov3.cfg"
-    weightsfile = "yolov3.weights"
-    num_classes = 80
+        cfgfile = "cfg/yolov3.cfg"
+        weightsfile = "yolov3.weights"
+        num_classes = 80
 
-    args = arg_parse()
-    confidence = float(args.confidence)
-    nms_thesh = float(args.nms_thresh)
-    start = 0
-    CUDA = torch.cuda.is_available()
+        args = arg_parse()
+        confidence = float(args.confidence)
+        nms_thesh = float(args.nms_thresh)
+        start = 0
+        CUDA = torch.cuda.is_available()
     
 
     
     
-    num_classes = 80
-    bbox_attrs = 5 + num_classes
+        num_classes = 80
+        bbox_attrs = 5 + num_classes
     
-    model = Darknet(cfgfile)
-    model.load_weights(weightsfile)
+        model = Darknet(cfgfile)
+        model.load_weights(weightsfile)
     
-    model.net_info["height"] = args.reso
-    inp_dim = int(model.net_info["height"])
+        model.net_info["height"] = args.reso
+        inp_dim = int(model.net_info["height"])
     
-    assert inp_dim % 32 == 0 
-    assert inp_dim > 32
+        assert inp_dim % 32 == 0
+        assert inp_dim > 32
 
-    if CUDA:
-        model.cuda()
+        if CUDA:
+            model.cuda()
             
-    model.eval()
+        model.eval()
     
-    videofile = 'video.avi'
+        videofile = 'video.avi'
     
-    cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(cameraChoice)
     
-    assert cap.isOpened(), 'Cannot capture source'
+        assert cap.isOpened(), 'Cannot capture source'
     
-    frames = 0
-    start = time.time()    
-    while cap.isOpened():
+        frames = 0
+        start = time.time()
+        while cap.isOpened():
         
-        ret, frame = cap.read()
-        if ret:
+            ret, frame = cap.read()
+            if ret:
             
-            img, orig_im, dim = prep_image(frame, inp_dim)
+                img, orig_im, dim = prep_image(frame, inp_dim)
             
-            im_dim = torch.FloatTensor(dim).repeat(1,2)                        
-            
-            
-            if CUDA:
-                im_dim = im_dim.cuda()
-                img = img.cuda()
+                im_dim = torch.FloatTensor(dim).repeat(1,2)
             
             
-            output = model(Variable(img), CUDA)
-            output = write_results(output, confidence, num_classes, nms = True, nms_conf = nms_thesh)
+                if CUDA:
+                    im_dim = im_dim.cuda()
+                    img = img.cuda()
+            
+            
+                output = model(Variable(img), CUDA)
+                output = write_results(output, confidence, num_classes, nms = True, nms_conf = nms_thesh)
 
-            if type(output) == int:
-                frames += 1
-                print("FPS of the video is {:5.2f}".format( frames / (time.time() - start)))
+                if type(output) == int:
+                    frames += 1
+                    print("FPS of the video is {:5.2f}".format( frames / (time.time() - start)))
+                    cv2.imshow("frame", orig_im)
+                    key = cv2.waitKey(1)
+                    if key & 0xFF == ord('q'):
+                        break
+                    continue
+            
+
+        
+                output[:,1:5] = torch.clamp(output[:,1:5], 0.0, float(inp_dim))/inp_dim
+            
+#               im_dim = im_dim.repeat(output.size(0), 1)
+                output[:,[1,3]] *= frame.shape[1]
+                output[:,[2,4]] *= frame.shape[0]
+
+            
+                classes = load_classes('data/coco.names')
+                colors = pkl.load(open("pallete", "rb"))
+            
+                list(map(lambda x: write(x, orig_im), output))
+            
+            
                 cv2.imshow("frame", orig_im)
                 key = cv2.waitKey(1)
                 if key & 0xFF == ord('q'):
                     break
-                continue
-            
-
-        
-            output[:,1:5] = torch.clamp(output[:,1:5], 0.0, float(inp_dim))/inp_dim
-            
-#            im_dim = im_dim.repeat(output.size(0), 1)
-            output[:,[1,3]] *= frame.shape[1]
-            output[:,[2,4]] *= frame.shape[0]
+                frames += 1
+                print("FPS of the video is {:5.2f}".format( frames / (time.time() - start)))
 
             
-            classes = load_classes('data/coco.names')
-            colors = pkl.load(open("pallete", "rb"))
-            
-            list(map(lambda x: write(x, orig_im), output))
-            
-            
-            cv2.imshow("frame", orig_im)
-            key = cv2.waitKey(1)
-            if key & 0xFF == ord('q'):
+            else:
                 break
-            frames += 1
-            print("FPS of the video is {:5.2f}".format( frames / (time.time() - start)))
-
-            
-        else:
-            break
     
-
+    else:
+        exit()
     
     
 
